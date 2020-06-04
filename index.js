b658ddade478606ff1e6bf0defbba291a1c9ad13
Suemin Cho <sm991105@gmail.com>
2020-06-04 18:57:46 +0900

    fixing erros

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("./db/mongoose"); // holy fucking shit !!!!!!
 const User = require("./db/models/User");
 const app = express();
 const cookieParser = require("cookie-parser");
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -75,11 +74,14 @@ app.get("/api/users/authenticate", async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
 
-app.listen(port, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log(`Server is up on port ${port}.`);
 });
